b6280e440a53ed42e575c0516a87777fa2cc2268
vignesh220402 <skvigneshsivakumar@gmail.com>
2025-02-19 15:33:39 +0530

    Come onn!

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import mylogo from '../assets/logo.jpg';
 
-
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -31,13 +30,13 @@ const Navbar = () => {
         </button>
         <div id="anchor" className={`navbar-links-container ${menuOpen ? 'open' : ''}`}>
           <ul className="navbar-links">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/services">Services</Link></li>
-            <li><Link to="/Blog">Blog</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
+            <li><Link to="/about" onClick={toggleMenu}>About</Link></li>
+            <li><Link to="/services" onClick={toggleMenu}>Services</Link></li>
+            <li><Link to="/Blog" onClick={toggleMenu}>Blog</Link></li>
+            <li><Link to="/contact" onClick={toggleMenu}>Contact</Link></li>
           </ul>
-          <Link to="/contact" className="contact-button">Contact Us</Link>
+          <Link to="/contact" className="contact-button" onClick={toggleMenu}>Contact Us</Link>
         </div>
       </nav>
     </div>
